bee8f8b22eb1ed772a31c88520a965023bbf88f0
John Cairns <john@2ad.com>
2020-11-18 13:43:14 -0600

    fix cancel

--- cancel_order.py
+++ cancel_order.py
@@ -4,16 +4,17 @@ import sys,time,json
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         try:
             apikeyfile = sys.argv[1]
+            orderId = sys.argv[2]
             with open(apikeyfile) as keyfile:
                 apikey = json.load(keyfile)
                 orderapi = binance_order(apikey)
 
                 order_param = {
                     'symbol': 'BTCUSDT',
-                    'orderId': 317834,
+                    'orderId': orderId,
                     'timestamp': int(time.time()-86400)
                 }
 
@@ -23,4 +24,4 @@ if __name__ == '__main__':
         except Exception as e:
             print("Failed. "+str(e))
     else:
-        print('apikey is required')
+        print('apikey and orderid are required')
